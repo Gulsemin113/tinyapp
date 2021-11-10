6e75356d44e0c8f551625a980e80b0dce207308f
Gulsemin113 <gulseminyildiz@hotmail.com>
2021-11-10 05:04:58 +0000

    Commit before the merge

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-
+const cookieParser = require('cookie-parser')
 const randomString = () => {
   let random = Math.random().toString(36).substring(2,8);
   return random;
@@ -10,6 +10,7 @@ const randomString = () => {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,7 +22,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  console.log(req.cookies);
+  let templateVars = {
+    username : req.cookies['username'],
+    urls : urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -45,9 +50,12 @@ app.get("/u/:shortURL", (req, res) => {
 
 
 app.get('/urls/:shortURL', (req,res) => {
+  console.log(req.cookies);
   let templateVars = {
+    username : req.cookies['username'],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.param.shortURL]
+
   };
   res.render("urls_show", templateVars);
 });
@@ -79,6 +87,18 @@ app.post('/urls', (req,res) => {
   const shortURL = randomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
+});
+
+//The Login Route
+app.post('/login', (req,res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+//The Logout Route
+app.post('/logout', (req,res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
